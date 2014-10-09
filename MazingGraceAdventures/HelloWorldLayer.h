eea2b938ed33d85510a3ea57934ae7b33d744d1d
Ron Coleman <ronncoleman@gmail.com>
2014-10-09 10:24:05 -0400

    Initial Commit

---
+++ MazingGraceAdventures/HelloWorldLayer.h
@@ -0,0 +1,23 @@
+//
+//  HelloWorldLayer.h
+//  MazingGraceAdventures
+//
+//  Created by Ronald G Coleman on 10/9/14.
+//  Copyright __MyCompanyName__ 2014. All rights reserved.
+//
+
+
+#import <GameKit/GameKit.h>
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
---
+++ MazingGraceAdventures/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MazingGraceAdventures
+//
+//  Created by Ronald G Coleman on 10/9/14.
+//  Copyright __MyCompanyName__ 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
